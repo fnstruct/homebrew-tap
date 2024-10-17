07898c8169fea5b1c4188c09e5576dc4091b3b4f
fnstruct <184018508+fnstruct@users.noreply.github.com>
2024-10-16 20:15:00 -0400

    feat: ris 1.1.0

--- Formula/r/ris.rb
+++ Formula/r/ris.rb
@@ -1,8 +1,8 @@
 class Ris < Formula
   desc "Rename in sequence"
   homepage "https://github.com/fnstruct/ris"
-  url "https://github.com/fnstruct/ris/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "dee446a3e3614d0ff900d399c52b6702ca25c3dec8648e9f01c0e6240b2e971b"
+  url "https://github.com/fnstruct/ris/archive/refs/tags/1.1.0.tar.gz"
+  sha256 "0a1dba2efe534878c82fd3aba78e83e7efcec729f86c97be4f2f35144a3d42a9"
   license "BSD-3-Clause"
   head "https://github.com/fnstruct/ris.git", branch: "master"
 
@@ -14,6 +14,6 @@ class Ris < Formula
   end
 
   test do
-    assert_match "ris \"#{version}\"", shell_output("#{bin}/ris -version")
+    assert_match "ris #{version}", shell_output("#{bin}/ris -version")
   end
 end
