65b77c5dcd33caf09ca93cca70d136293f54de66
fnstruct <184018508+fnstruct@users.noreply.github.com>
2024-10-20 17:22:00 -0400

    ris: add 1.2.0 bottle

--- Formula/r/ris.rb
+++ Formula/r/ris.rb
@@ -6,6 +6,13 @@ class Ris < Formula
   license "BSD-3-Clause"
   head "https://github.com/fnstruct/ris.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/fnstruct/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b514f17f90451fcdd7b2f158f67e466e9d536bff35bbb88649b7eeb8322635fa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b514f17f90451fcdd7b2f158f67e466e9d536bff35bbb88649b7eeb8322635fa"
+    sha256 cellar: :any_skip_relocation, ventura:       "5c4cb96a560ad19de07ab05dcd89d1dc0aff1888938b03caee27ba6dc34a440d"
+  end
+
   depends_on "go" => :build
 
   def install
